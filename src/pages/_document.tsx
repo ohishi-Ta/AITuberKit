6106a600fb73c27bbb52d9a5e0a18ed1424185cb
ohishi-Ta <lumiere.zzz03918@gmail.com>
2025-08-08 18:27:24 +0900

    修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-       <body style={{ backgroundColor: 'transparent' }}>
+      <body style={{ backgroundColor: 'transparent' }}>
         <Main />
         <NextScript />
       </body>
